5fc15bc50bd6ece98055f581846a50cdb716869d
Patryk Rubiniak <patryk.rubiniak@gmail.com>
2023-10-24 02:33:36 +0200

    Zadanie domowe dzień 4

--- 21App/21App/Program.cs
+++ 21App/21App/Program.cs
@@ -1,1 +1,35 @@
-﻿Console.WriteLine("Hello, World!");
+﻿var Name = "Ewa";
+var Sex = "Kobieta";
+var Age = 33;
+
+if (Sex == "Kobieta" && Name == "Ewa" && Age == 33)
+{
+    Console.WriteLine("Ewa, 33 lat");
+}
+else if (Age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (Age > 30)
+{
+    Console.WriteLine("Kobieta powyżej 30 lat");
+}
+else if (Age < 30 || Sex != "Kobieta" || Name != "Ewa")
+{
+    Console.WriteLine("To nie jest Ewa");
+}
+
+if (Age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+
+else
+{
+    Console.WriteLine("Pełnoletni mężczyzna");
+}
+
+
+
+
+

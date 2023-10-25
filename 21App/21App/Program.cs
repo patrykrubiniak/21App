b6fb0890ed0d9b02534bf992f8f31d5130ca416f
Patryk Rubiniak <patryk.rubiniak@gmail.com>
2023-10-26 01:14:24 +0200

    Zadanie domowe dzień 6

--- 21App/21App/Program.cs
+++ 21App/21App/Program.cs
@@ -1,68 +1,43 @@
-﻿int number = 4566;
-string numberInString = number.ToString();
-char[] letters = numberInString.ToArray();
+﻿using _21App;
 
-int counter0 = 0;
-int counter1 = 0;
-int counter2 = 0;
-int counter3 = 0;
-int counter4 = 0;
-int counter5 = 0;
-int counter6 = 0;
-int counter7 = 0;
-int counter8 = 0;
-int counter9 = 0;
+Employee employee1 = new Employee("Karol", "Kowalski", 21);
+Employee employee2 = new Employee("Adam", "Nowak", 34);
+Employee employee3 = new Employee("Eliza", "Miś", 28);
 
-foreach (char letter in letters)
+employee1.AddScore(7);
+employee1.AddScore(1);
+employee1.AddScore(4);
+employee1.AddScore(3);
+employee1.AddScore(5);
+
+employee2.AddScore(2);
+employee2.AddScore(5);
+employee2.AddScore(9);
+employee2.AddScore(3);
+employee2.AddScore(4);
+
+employee3.AddScore(9);
+employee3.AddScore(3);
+employee3.AddScore(5);
+employee3.AddScore(9);
+employee3.AddScore(10);
+
+List<Employee> employees = new List<Employee>()
+
 {
-    if (letter == '0')
+    employee1, employee2, employee3
+};
+
+int maxResult = -1;
+Employee employeeWithMaxResult = null;
+
+foreach (var employee in employees)
+{
+    if (employee.Result > maxResult)
     {
-        counter0++;
-    }
-    else if (letter == '1')
-    {
-        counter1++;
-    }
-    else if (letter == '2')
-    {
-        counter2++;
-    }
-    else if (letter == '3')
-    {
-        counter3++;
-    }
-    else if (letter == '4')
-    {
-        counter4++;
-    }
-    else if (letter == '5')
-    {
-        counter5++;
-    }
-    else if (letter == '6')
-    {
-        counter6++;
-    }
-    else if (letter == '7')
-    {
-        counter7++;
-    }
-    else if (letter == '8')
-    {
-        counter8++;
-    }
-    else if (letter == '9')
-    {
-        counter9++;
+        employeeWithMaxResult = employee;
     }
 }
-Console.WriteLine("0=" + counter0);
-Console.WriteLine("1=" + counter1);
-Console.WriteLine("2=" + counter2);
-Console.WriteLine("3=" + counter3);
-Console.WriteLine("4=" + counter4);
-Console.WriteLine("5=" + counter5);
-Console.WriteLine("6=" + counter6);
-Console.WriteLine("7=" + counter7);
-Console.WriteLine("8=" + counter8);
-Console.WriteLine("9=" + counter9);
+
+Console.WriteLine(employeeWithMaxResult.Name + " " + employeeWithMaxResult.Surname + " " + "w wieku" + " " + employeeWithMaxResult.Age + " " + "lat");
+Console.WriteLine("Z wynikiem" + " " + employeeWithMaxResult.Result + " " + "pkt.");

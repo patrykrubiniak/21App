9cdb6c2c37b8637fb6043d309d52245078159d5e
Patryk Rubiniak <patryk.rubiniak@gmail.com>
2023-11-01 19:31:13 +0100

    Zadanie domowe dzień 10

--- 21App/21App/Employee.cs
+++ 21App/21App/Employee.cs
@@ -1,4 +1,6 @@
-﻿namespace _21App;
+﻿using System.ComponentModel.Design;
+
+namespace _21App;
 public class Employee
 {
     public List<float> grades = new List<float>();
@@ -7,13 +9,47 @@ public class Employee
         this.Name = name;
         this.Surname = surname;
     }
-
     public string Name { get; private set; }
     public string Surname { get; private set; }
-
     public void AddGrades(float grade)
     {
-        this.grades.Add(grade);
+        if (grade >= 0 && grade <= 100)
+
+            this.grades.Add(grade);
+
+        else
+        {
+            Console.WriteLine("Niepoprawne dane");
+        }
+    }
+
+    public void AddGrades(string grade)
+    {
+        if (float.TryParse(grade, out float result))
+        {
+            this.AddGrades(result);
+        }
+
+        else
+        {
+            Console.WriteLine("String to nie jest float");
+        }
+    }
+    public void AddGrades(double grade)
+    {
+        float value = (float)grade;
+        this.AddGrades(value);
+    }
+    public void AddGrades(int grade)
+    {
+        float value = grade;
+        this.AddGrades(value);
+    }
+
+    public void AddGrades(long grade)
+    {
+        float value = grade;
+        this.AddGrades(value);
     }
 
     public Statistics GetStatistics()
@@ -23,7 +59,7 @@ public class Employee
         statistics.Max = float.MinValue;
         statistics.Min = float.MaxValue;
 
-        foreach (var grade in this.grades) 
+        foreach (var grade in this.grades)
         {
             statistics.Max = Math.Max(statistics.Max, grade);
             statistics.Min = Math.Min(statistics.Min, grade);
@@ -31,7 +67,7 @@ public class Employee
         }
 
         statistics.Average /= this.grades.Count;
-        
+
         return statistics;
     }
 }

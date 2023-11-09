c33cbe7a6b631c5ddc60efa86e5172be303ce3ab
Patryk Rubiniak <patryk.rubiniak@gmail.com>
2023-11-09 19:16:03 +0100

    Aplikacja do oceny jednego pracownika

--- 21App/21App/Employee.cs
+++ 21App/21App/Employee.cs
@@ -1,19 +1,15 @@
 ﻿using System.ComponentModel.Design;
 
 namespace _21App;
-public class Employee
+public class Employee : Person
 {
     private List<float> grades = new List<float>();
 
-    public Employee()
+    public Employee (string name, string surname, char gender, int age)
+        :base (name, surname, gender, age)
     { 
-    }
-    public Employee(string name, string surname)
-    {
-        this.Name = name;
-        this.Surname = surname;
-    }
 
+    }   
     public string Name { get; private set; }
     public string Surname { get; private set; }
 
